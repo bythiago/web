2a09de6af8f9796cac40322818610add4a6bc99d
Thiago Vieira <thiiagovieir@gmail.com>
2016-03-08 23:50:16 -0300

    comentário

--- meuprojeto/pessoa/forms.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django import forms
-from models import Pessoa
-
-class CadastroForm(forms.ModelForm):
-	class Meta:
-		model = Pessoa
-		fields = ('primeiro_nome' , 'ultimo_nome')
