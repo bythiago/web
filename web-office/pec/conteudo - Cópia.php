108bb60e91983b0c76dc66f9d1cc82a77b63b4c1
THIAGO VIEIRA DE CARVALHO <thiago.vieira@ad.prefeitura.intranet>
2016-04-14 15:04:30 -0300

    Trabalho pec 2016

---
+++ web-office/pec/conteudo - Cópia.php
@@ -0,0 +1,184 @@
+<header>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+    <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+    <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img class="first-slide" src="http://cavi.com.br/wp-content/uploads/2015/09/criancas-felizes-wallpaper-610x310.jpg" alt="First slide">
+      <div class="container">
+        <div class="carousel-caption">
+          <h2>Projeto de Extensão à Comunidade</h2>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p><br>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <img class="second-slide" src="http://www.wallpaperhd.pk/wp-content/uploads/2015/08/world-of-travel-wallpaper.jpg" alt="Second slide">
+      <div class="container">
+        <div class="carousel-caption">
+          <h2>Geografia Legal</h2>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p><br>
+        </div>
+      </div>
+    </div>
+    <!--div class="item">
+      <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+      <div class="container">
+        <div class="carousel-caption">
+          <h1>asd</h1>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+        </div>
+      </div>
+    </div-->
+  </div>
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</header>
+
+<section id="services">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
+        <img src="https://8f9bb20db99e994b326a79b5913891492ed7718c.googledrive.com/host/0Bz_rICHLHzJzZkVndU41QllNWE0/images/icons/flag.png" alt="">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p>
+        <a href="http://www.jogos-geograficos.com/jogos-geografia-Bandeiras-do-Mundo-_pageid100.html" target="_blank" class="btn btn-success btn-block wow bounce">Bandeiras do Mundo</a>
+      </div>
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
+        <img src="https://8f9bb20db99e994b326a79b5913891492ed7718c.googledrive.com/host/0Bz_rICHLHzJzZkVndU41QllNWE0/images/icons/search.png" alt="">
+        <p>Posicione os pontos cardeais na rosa dos ventos.</p>
+        <a href="http://www.sogeografia.com.br/Jogos/rosaventos.html" target="_blank" class="btn btn-success btn-block wow pulse">Rosa dos Ventos</a>
+      </div>
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
+        <img src="https://8f9bb20db99e994b326a79b5913891492ed7718c.googledrive.com/host/0Bz_rICHLHzJzZkVndU41QllNWE0/images/icons/world-map.png" alt="">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p>
+        <a href="http://www.jogos-geograficos.com/jogos-geografia-Estados-do-Brasil-_pageid41.html" target="_blank" class="btn btn-success btn-block wow jello">Estados do Brasil</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- <section class="fix"></section> -->
+
+<!--section>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="timeline">
+        <dl>
+          <dt>
+            Apr 2014
+          </dt>
+          <dd class="pos-right clearfix">
+            <div class="circ">
+            </div>
+            <div class="time">
+              Apr 14
+            </div>
+            <div class="events">
+              <div class="pull-left">
+                <img class="events-object img-rounded wow bounce" src="http://bootflat.github.io/img/photo-1.jpg">
+              </div>
+              <div class="events-body">
+                <h4 class="events-heading">
+                  Bootstrap
+                </h4>
+                <p>
+                  Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt
+                  tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor,
+                  williamsburg carles vegan helvetica.
+                </p>
+              </div>
+            </div>
+          </dd>
+          <dd class="pos-left clearfix">
+            <div class="circ">
+            </div>
+            <div class="time">
+              Apr 10
+            </div>
+            <div class="events">
+              <div class="pull-left">
+                <img class="events-object img-rounded wow bounce" src="http://bootflat.github.io/img/photo-2.jpg">
+              </div>
+              <div class="events-body">
+                <h4 class="events-heading">
+                  Bootflat
+                </h4>
+                <p>
+                  Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt
+                  tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor,
+                  williamsburg carles vegan helvetica.
+                </p>
+              </div>
+            </div>
+          </dd>
+          <dt>
+            Mar 2014
+          </dt>
+          <dd class="pos-right clearfix">
+            <div class="circ">
+            </div>
+            <div class="time">
+              Mar 15
+            </div>
+            <div class="events">
+              <div class="pull-left">
+                <img class="events-object img-rounded wow bounce" src="http://bootflat.github.io/img/photo-3.jpg">
+              </div>
+              <div class="events-body">
+                <h4 class="events-heading">
+                  Flat UI
+                </h4>
+                <p>
+                  Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt
+                  tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor,
+                  williamsburg carles vegan helvetica.
+                </p>
+              </div>
+            </div>
+          </dd>
+          <dd class="pos-left clearfix">
+            <div class="circ">
+            </div>
+            <div class="time">
+              Mar 8
+            </div>
+            <div class="events">
+              <div class="pull-left">
+                <img class="events-object img-rounded wow bounce" src="http://bootflat.github.io/img/photo-4.jpg">
+              </div>
+              <div class="events-body">
+                <h4 class="events-heading">
+                  UI design
+                </h4>
+                <p>
+                  Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt
+                  tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor,
+                  williamsburg carles vegan helvetica.
+                </p>
+              </div>
+            </div>
+          </dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+</div>
+</section-->
---
+++ web-office/pec/src/index - Cópia.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap Themes</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="assets/components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/components/bootstrap/assets/components/Bootflat/bootflat/css/bootflat.min.css" rel="stylesheet">
+  <link href="dist/css/docs.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="http://ironsummitmedia.github.io/startbootstrap-creative/font-awesome/css/font-awesome.min.css" type="text/css">
+  <link href="dist/css/main.min.css" rel="stylesheet">
+  <link href="assets/components/animate.css/animate.min.css" rel="stylesheet">
+  <link href="http://ironsummitmedia.github.io/startbootstrap-scrolling-nav/css/scrolling-nav.css" rel="stylesheet">
+  <!-- Google Fonts | Lato -->
+  <link href='https://fonts.googleapis.com/css?family=Lato|Alegreya+Sans' rel='stylesheet' type='text/css'>
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-fixed-top navbar-custom" role="navigation">
+  <div class="container">
+    <div class="navbar-header page-scroll">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+        <span class="sr-only">
+          Toggle navigation
+        </span>
+        <span class="icon-bar">
+        </span>
+        <span class="icon-bar">
+        </span>
+        <span class="icon-bar">
+        </span>
+      </button>
+      <!-- <a class="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a> -->
+      <div class="nav-logo">
+        <a href="#"><img src="http://themefisher.com/themes/small-apps/img/logo.png" alt="logo"></a>
+      </div>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+      <ul class="nav navbar-nav">
+        <!-- Hidden li included to remove active class from about link when scrolled
+        up past about section -->
+        <li class="hidden">
+          <a class="page-scroll" href="#page-top"></a>
+        </li>
+        <li>
+          <a class="page-scroll" href="#about">Sobre</a>
+        </li>
+        <li>
+          <a class="page-scroll" href="#services">Serviços</a>
+        </li>
+        <li>
+          <a class="page-scroll" href="#map">Contato</a>
+        </li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse -->
+  </div>
+  <!-- /.container -->
+</nav>
+<div class="container-fluid">
+  <div class="row">
+    <header>
+      <div class="header-content">
+        <h1>Your Favorite Source of Free Bootstrap Themes</h1>
+        <hr>
+      </div>
+    </header>
+  </div>
+</div>
+
+<section id="services">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
+        <img src="https://8f9bb20db99e994b326a79b5913891492ed7718c.googledrive.com/host/0Bz_rICHLHzJzZkVndU41QllNWE0/images/icons/flag.png" alt="">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p>
+        <a href="#" class="btn btn-success btn-block wow bounce">button</a>
+      </div>
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
+        <img src="https://8f9bb20db99e994b326a79b5913891492ed7718c.googledrive.com/host/0Bz_rICHLHzJzZkVndU41QllNWE0/images/icons/search.png" alt="">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p>
+        <a href="#" class="btn btn-success btn-block wow pulse">button</a>
+      </div>
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
+        <img src="https://8f9bb20db99e994b326a79b5913891492ed7718c.googledrive.com/host/0Bz_rICHLHzJzZkVndU41QllNWE0/images/icons/world-map.png" alt="">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p>
+        <a href="#" class="btn btn-success btn-block wow jello">button</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- <section class="fix"></section> -->
+
+<section>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="timeline">
+        <dl>
+          <dt>
+            Apr 2014
+          </dt>
+          <dd class="pos-right clearfix">
+            <div class="circ">
+            </div>
+            <div class="time">
+              Apr 14
+            </div>
+            <div class="events">
+              <div class="pull-left">
+                <img class="events-object img-rounded wow bounce" src="http://bootflat.github.io/img/photo-1.jpg">
+              </div>
+              <div class="events-body">
+                <h4 class="events-heading">
+                  Bootstrap
+                </h4>
+                <p>
+                  Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt
+                  tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor,
+                  williamsburg carles vegan helvetica.
+                </p>
+              </div>
+            </div>
+          </dd>
+          <dd class="pos-left clearfix">
+            <div class="circ">
+            </div>
+            <div class="time">
+              Apr 10
+            </div>
+            <div class="events">
+              <div class="pull-left">
+                <img class="events-object img-rounded wow bounce" src="http://bootflat.github.io/img/photo-2.jpg">
+              </div>
+              <div class="events-body">
+                <h4 class="events-heading">
+                  Bootflat
+                </h4>
+                <p>
+                  Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt
+                  tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor,
+                  williamsburg carles vegan helvetica.
+                </p>
+              </div>
+            </div>
+          </dd>
+          <dt>
+            Mar 2014
+          </dt>
+          <dd class="pos-right clearfix">
+            <div class="circ">
+            </div>
+            <div class="time">
+              Mar 15
+            </div>
+            <div class="events">
+              <div class="pull-left">
+                <img class="events-object img-rounded wow bounce" src="http://bootflat.github.io/img/photo-3.jpg">
+              </div>
+              <div class="events-body">
+                <h4 class="events-heading">
+                  Flat UI
+                </h4>
+                <p>
+                  Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt
+                  tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor,
+                  williamsburg carles vegan helvetica.
+                </p>
+              </div>
+            </div>
+          </dd>
+          <dd class="pos-left clearfix">
+            <div class="circ">
+            </div>
+            <div class="time">
+              Mar 8
+            </div>
+            <div class="events">
+              <div class="pull-left">
+                <img class="events-object img-rounded wow bounce" src="http://bootflat.github.io/img/photo-4.jpg">
+              </div>
+              <div class="events-body">
+                <h4 class="events-heading">
+                  UI design
+                </h4>
+                <p>
+                  Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt
+                  tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor,
+                  williamsburg carles vegan helvetica.
+                </p>
+              </div>
+            </div>
+          </dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
+<div id="map"></div>
+
+<section id="contato">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-8">
+        <div class="row form-group">
+          <div class="col-xs-3">
+            <input type="text" class="form-control" id="firstName" name="firstName"
+            placeholder="First Name" required="">
+          </div>
+          <div class="col-xs-3">
+            <input type="text" class="form-control" id="middleName" name="firstName"
+            placeholder="Middle Name" required="">
+          </div>
+          <div class="col-xs-4">
+            <input type="text" class="form-control" id="lastName" name="lastName"
+            placeholder="Last Name" required="">
+          </div>
+        </div>
+        <div class="row form-group">
+          <div class="col-xs-5">
+            <input type="email" class="form-control" name="email" placeholder="Email"
+            required="">
+          </div>
+          <div class="col-xs-5">
+            <input type="email" class="form-control" name="phone" placeholder="Phone"
+            required="">
+          </div>
+        </div>
+        <div class="row form-group">
+          <div class="col-xs-10">
+            <input type="homepage" class="form-control" placeholder="Website URL"
+            required="">
+          </div>
+        </div>
+        <div class="row form-group">
+          <div class="col-xs-10">
+            <button class="btn btn-default pull-right">
+              Contact Us
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3 pull-right">
+        <address>
+          <strong>
+            Iatek, LLC.
+          </strong>
+          <br>
+          795 Folsom Ave, Suite 600
+          <br>
+          Newport, RI 94107
+          <br>
+          P: (123) 456-7890
+        </address>
+        <address>
+          <strong>
+            Email Us
+          </strong>
+          <br>
+          <a href="mailto:#">first.last@example.com</a>
+        </address>
+      </div>
+    </div>
+    <!--/row-->
+  </div>
+</section>
+
+<footer id="footer">
+<div class="footer-copyright">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2">
+        <a class="logo" href="index.html">
+          <img src="http://scripteden.com/previews/cupid/images/logo.png" alt="Template Eden">
+      </a>
+      </div>
+      <div class="col-md-5">
+        <p>
+          © Copyright 2015 by Script Eden. All Rights Reserved.
+        </p>
+      </div>
+      <div class="col-md-5">
+        <nav id="footer-menu">
+          <ul>
+            <li>
+              <a href="#">FAQ's</a>
+            </li>
+            <li>
+              <a href="#">Sitemap</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</div>
+</footer>
+
+<script>
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -22.5082204, lng: -44.08973},
+      zoom: 15,
+      zoomControl: false,
+      scaleControl: false,
+      scrollwheel: false,
+      disableDoubleClickZoom: true,
+    }); 
+
+
+    var marker = new google.maps.Marker({
+      position: map.center,
+      map: map,
+      title: 'UGB - Centro Universitário Geraldo Di Biase'
+    });
+  }
+</script>
+
+<!-- jQuery -->
+<script src="assets/components/jquery/dist/jquery.js"></script>
+<!-- Bootstrap JavaScript -->
+<script src="assets/components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- wow.min.js -->
+<script src="http://ironsummitmedia.github.io/startbootstrap-creative/js/jquery.fittext.js"></script>
+<script src="http://ironsummitmedia.github.io/startbootstrap-creative/js/wow.min.js"></script>
+<script src="http://ironsummitmedia.github.io/startbootstrap-creative/js/creative.js"></script>
+<!-- scrolling-nav -->
+<script src="http://ironsummitmedia.github.io/startbootstrap-scrolling-nav/js/jquery.easing.min.js"></script>
+<script src="http://ironsummitmedia.github.io/startbootstrap-scrolling-nav/js/scrolling-nav.js"></script>
+<!-- Google Map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7xvgiqL3Dhz14Ovx-ysjMud6OX3VzK2k&callback=initMap" async defer></script>
+</body>
+</html>
