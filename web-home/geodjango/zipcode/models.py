f874c70000574441af1f575334979cab3ca70663
Thiago Vieira <thiago.vieira@epdvr.com.br>
2016-05-04 18:02:37 -0300

    geodengue

--- web-home/geodjango/zipcode/models.py
+++ web-home/geodjango/zipcode/models.py
@@ -1,11 +1,3 @@
-from __future__ import unicode_literals
+from django.db import models
 
-from django.contrib.gis.db import models
-
-class Zipcode(models.Model):
-    code = models.CharField(max_length=5)
-    poly = models.PolygonField()
-
-class Elevation(models.Model):
-    name = models.CharField(max_length=100)
-    rast = models.RasterField()
+# Create your models here.
