97b226f3e4122fa19764137354df39bf8e86c4cd
Thiago Vieira <thiago.vieira@epdvr.com.br>
2016-05-04 18:28:50 -0300

    ***

--- web-office/geodengue/mundo/views.py
+++ web-office/geodengue/mundo/views.py
@@ -4,6 +4,7 @@ from .models import WorldBorder
 
 # Create your views here.
 def index(request):
-	paises = WorldBorder.objects.raw("SELECT id, name, area, pop2005, fips, iso2, iso3, un, region, subregion, lon, lat, mpoly FROM mundo_worldborder where name = %s", ['Brazil'])
+	#paises = WorldBorder.objects.raw("SELECT id, name,area, pop2005, fips, iso2, iso3, un, region, subregion, lon, lat, mpoly FROM mundo_worldborder where name = %s", ['Brazil'])
+	paises = WorldBorder.objects.raw("SELECT id, name,area, pop2005, fips, iso2, iso3, un, region, subregion, lon, lat, mpoly FROM mundo_worldborder", 0)
 	context = {'paises': paises}
 	return render(request, 'index.html' , context)
